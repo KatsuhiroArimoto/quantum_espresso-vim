ffd7bf1e44a4ec5c5e04a84a1e0b81d6561e9596
KatsuhiroArimoto <75106395+KatsuhiroArimoto@users.noreply.github.com>
2022-03-01 15:44:39 +0900

    Minor shortening of source code

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!usr/bin/env bash
 
-mkdir ~/.vim
-mkdir ~/.vim/ftdetect
-mkdir ~/.vim/syntax
-cp ftdetect/espresso.vim ~/.vim/ftdetect/espresso.vim
-cp syntax/espresso.vim ~/.vim/syntax/espresso.vim
+mkdir -p $HOME/.vim/ftdetect $HOME/.vim/syntax
+cp ./ftdetect/espresso.vim $HOME/.vim/ftdetect/espresso.vim
+cp ./syntax/espresso.vim $HOME/.vim/syntax/espresso.vim
